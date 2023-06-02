e9b0097e7d39780f8385e398d3dfe88cc86ac152
Joacir Sampaio <joacirprog@gmail.com>
2023-06-02 11:55:11 -0300

    finished

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,7 +1,25 @@
 import { Category } from '../types/Category'
 
 export const categories: Category = {
-  food: { title: 'Alimentação', color: 'blue', expense: true },
-  rent: { title: 'Aluguel', color: 'brown', expense: true },
+  rent: { title: 'Moradia', color: 'blue', expense: true },
+  relex: { title: 'Entretenimento', color: 'blue', expense: true },
+  transport: { title: 'Transporte', color: 'blue', expense: true },
+  loans: { title: 'Empréstimo', color: 'blue', expense: true },
+  safe: { title: 'Seguro', color: 'blue', expense: true },
+  taxes: { title: 'Impostos', color: 'blue', expense: true },
+  food: { title: 'Comida', color: 'brown', expense: true },
+  savingsOrinvestments: {
+    title: 'Poupanças ou investimentos',
+    color: 'blue',
+    expense: true,
+  },
+  pets: { title: 'Animais de estimação', color: 'blue', expense: true },
+  giftsAndDonations: {
+    title: 'Presentes e doações',
+    color: 'blue',
+    expense: true,
+  },
+  personalCares: { title: 'Cuidados pessoais', color: 'blue', expense: true },
+  legalAdvice: { title: 'Assessoria jurídica', color: 'blue', expense: true },
   salary: { title: 'Salário', color: 'green', expense: false },
 }
--- src/data/items.ts
+++ src/data/items.ts
@@ -1,28 +1,3 @@
 import { Item } from '../types/Item'
 
-export const items: Item[] = [
-  {
-    date: new Date(2023, 5, 29),
-    category: 'food',
-    title: 'McDonalds',
-    value: 32.12,
-  },
-  {
-    date: new Date(2023, 5, 29),
-    category: 'food',
-    title: 'Burger King',
-    value: 28.0,
-  },
-  {
-    date: new Date(2023, 5, 10),
-    category: 'rent',
-    title: 'Aluguel Apt',
-    value: 500,
-  },
-  {
-    date: new Date(2023, 4, 15),
-    category: 'salary',
-    title: 'Salário Iquine',
-    value: 1800,
-  },
-]
+export const items: Item[] = []
